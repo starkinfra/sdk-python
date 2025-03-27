d210d899920720436cae1d3ccb2ab85aed5e43da
Lucas Cardozo <lucas.cardozo@starkbank.com>
2025-03-27 13:35:41 -0300

    Update CreditNote handling and add new test cases for date formats

--- tests/utils/date.py
+++ tests/utils/date.py
@@ -1,35 +1,38 @@
 from random import randint
 from datetime import datetime, timedelta, date
 
+def futureDateTime(days=7): # type (int) -> datetime:
+    return datetime.now() + timedelta(days=days)
 
-def futureDate(days=7):
+
+def futureDate(days=7): # type (int) -> datetime:
     return date.today() + timedelta(days=days)
 
 
-def randomFutureDate(days=7):
+def randomFutureDate(days=7): # type (int) -> datetime:
     return futureDate(days=randint(1, days))
 
 
-def randomFutureDatetime(days=7):
+def randomFutureDatetime(days=7): # type (int) -> datetime:
     return datetime.now() + timedelta(seconds=randint(1, days*24*3600))
 
 
-def randomPastDate(days=7):
+def randomPastDate(days=7): # type (int) -> datetime:
     return date.today() - timedelta(days=randint(1, days))
 
 
-def randomPastDatetime(days=7):
+def randomPastDatetime(days=7): # type (int) -> datetime:
     return datetime.now() - timedelta(seconds=randint(1, days*24*3600))
 
 
-def randomDateBetween(after, before):
+def randomDateBetween(after, before): # type: (date, date) -> date
     if after > before:
         after, before = before, after
     delta = before - after
     return after + timedelta(days=randint(0, delta.days))
 
 
-def randomDatetimeBetween(after, before):
+def randomDatetimeBetween(after, before): # type: (datetime, datetime) -> datetime
     if after > before:
         after, before = before, after
     delta = before - after

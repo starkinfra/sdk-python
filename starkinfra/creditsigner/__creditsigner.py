8cc457b7999b4a59c037e5edde333ded142ab503
Lucas Cardozo <lucas.cardozo@starkbank.com>
2025-04-15 17:31:23 -0300

    Add resend_token_one_signer function and corresponding tests to CreditSigner

--- starkinfra/creditsigner/__creditsigner.py
+++ starkinfra/creditsigner/__creditsigner.py
@@ -1,5 +1,7 @@
 from starkcore.utils.resource import Resource
 
+from starkinfra.utils import rest
+
 
 class CreditSigner(Resource):
     """# CreditSigner object
@@ -12,7 +14,7 @@ class CreditSigner(Resource):
     - id [string]: unique id returned when the CreditSigner is created. ex: "5656565656565656"
     """
 
-    def __init__(self, name, contact, method, id=None):
+    def __init__(self, name, contact, method, id=None, status=None):
         Resource.__init__(self, id=id)
         self.name = name
         self.contact = contact
@@ -20,3 +22,22 @@ class CreditSigner(Resource):
 
 
 _resource = {"class": CreditSigner, "name": "CreditSigner"}
+
+
+def resend_token_one_signer(signerId: str, user=None):
+    """# Resend token to signer 
+    Resend token to a specific signer.
+    ## Parameters (required):
+    - signerId [string]: object unique id. ex: "5656565656565656"
+    ## Parameters (optional):
+    - user [Organization/Project object, default None]: Organization or Project object. 
+    Not necessary if starkinfra.user was set before function call.
+    ## Return:
+    
+    """
+    return rest.patch_id(
+        resource=_resource,
+        id=signerId,
+        user=user,
+        payload={"isSent": False},
+    )

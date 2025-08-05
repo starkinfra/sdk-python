f4f19b4f0b1e7854b2cde3a4a5afabcece9c538e
Paulo Souza <paulo.souza@starkbank.com>
2025-08-05 16:10:19 -0300

    Add CreditNote payment method

--- starkinfra/creditnote/__creditnote.py
+++ starkinfra/creditnote/__creditnote.py
@@ -233,8 +233,20 @@ def cancel(id, user=None):
 
 
 def pdf(noteId, user=None):
-    """# Retrieve CCB disbursement
+    """# Retrieve CCB disbursement pdf
     Receive a CCB disbursement pdf file.
+    ## Parameters (required):
+    - noteId [string]: object unique id. ex: "5656565656565656"
+    ## Parameters (optional):
+    - user [Organization/Project object, default None]: Organization or Project object. Not necessary if starkinfra.user was set before function call.
+    ## Return:
+    - Transfer pdf file
+    """
+    return rest.get_content(resource=_resource, id=noteId, user=user, sub_resource_name="/pdf")
+
+def payment(noteId, user=None):
+    """# Retrieve CCB disbursement payment pdf
+    Receive a CCB disbursement payment pdf file.
     ## Parameters (required):
     - noteId [string]: object unique id. ex: "5656565656565656"
     ## Parameters (optional):
